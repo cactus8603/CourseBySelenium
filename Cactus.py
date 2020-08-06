8969d5d2cf62ca1561ce24b264932856d31b0b15
cactus8603 <cactus415646851@gmail.com>
2020-08-06 17:26:13 +0800

    init

--- Cactus.py
+++ Cactus.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait as wait  # for implicit and explict waits
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-import pyautogui
+from selenium.common.exceptions import NoSuchElementException
 import time
 import requests 
 from bs4 import BeautifulSoup
@@ -39,26 +39,10 @@ def getRand(driver):
     for t in a_tags:
         return t.text
     
-# 在NTNU端選課，刷新時間
-def refreshTimeNTNU(driver):
-    # 點選"校際選課"
-    driver.find_element_by_id("tab-1027-btnInnerEl").click()
 
-    # 等待彈出視窗並點選OK
-    time.sleep(1)
-    driver.find_element_by_id("button-1005-btnIconEl").click()
-
-    # 點選"師大選課"
-    driver.find_element_by_id("tab-1023-btnInnerEl").click()
-
-# 在NTU端選課，刷新時間
-def refreshTimeNTU(driver):
-    # 點選"師大選課"
-    driver.find_element_by_id("tab-1023-btnInnerEl").click()
-    
-    # 點選"校際選課"
-    time.sleep(1)
-    driver.find_element_by_id("tab-1027-btnInnerEl").click()
+def deleteTime(driver):
+    deleteTime = "var element = document.getElementById('now'); element.parentNode.removeChild(element);"
+    driver.execute_script(deleteTime)
 
 def login(driver, account, password):
 
@@ -80,116 +64,86 @@ def login(driver, account, password):
 
     # 登入
     driver.find_element_by_id("button-1016").click()
-
     time.sleep(2)
 
     # 點選"開始選課"
     driver.find_element_by_id("button-1017-btnIconEl").click()
 
 
-# 點選"登記"
-def clickAddClass():
-    time.sleep(1)
-
-    # 筆電
-    # pyautogui.click(75, 310)
-
-    # 赫彩
-    pyautogui.click(25, 270)
-
-    # 桌電
-    # pyautogui.click(x, y)
-
-    time.sleep(1)
-    pyautogui.click()
-    
-
-# 刪除時間限制
-def deleteTime():
-    time.sleep(1)
-    # 筆電
-    # pyautogui.click(158, 225)
-
-    # 赫彩
-    pyautogui.click(130, 125)
-
-    # 桌電
-    # pyautogui.click(x, y)
-
-    time.sleep(1)
-    pyautogui.rightClick()
-    time.sleep(1)
-    pyautogui.press('n')
-    time.sleep(1)
-    pyautogui.rightClick()
-    time.sleep(1)
-    pyautogui.press('n')
-    time.sleep(1)
-    pyautogui.press('backspace')
-
-    # 關閉devtools
-    pyautogui.hotkey('ctrl', 'shift', 'j')
-
-
-def selectCourseInNTNU(driver):
+def selectCourseInNTNU(driver, classSerialNumber):
+    # 基本資料
     account = '40747043S'
     password = 'cactus415646851'
     login(driver, account, password)
-    
-    # wait(driver, 1).until(EC.element_to_be_clickable((By.LINK_TEXT, "//span[text()='登記']"))).click()
-    # driver.find_element_by_id("add-btnInnerEl").click()
-    # element = driver.find_elements_by_css_selector('span')
-    # print(len(element))
-    # element[23].click()
-    # print(element)
-    # element[4].click()
-
-    # 點選"登記"
-    clickAddClass()
 
     # 刪除時間
-    deleteTime()
+    time.sleep(2)
+    deleteTime = "document.getElementById('now').remove();"
+    driver.execute_script(deleteTime)
 
-    # 點選到"開課序號"
-    pyautogui.click(175, 255)
-    pyautogui.rightClick()
-    time.sleep(1)
-    pyautogui.press('n')
-    time.sleep(1)
-    pyautogui.rightClick()
-    time.sleep(1)
-    pyautogui.press('n')
-    time.sleep(1)
+    # 切換frame
+    time.sleep(2)
+    driver.switch_to.frame("stfseldListDo")
+    
+    # 點選"查詢"
+    driver.find_element_by_id("query-btnInnerEl").click()
 
-    # 關閉devtools
-    pyautogui.hotkey('ctrl', 'shift', 'j')
-"""
+    #####選課畫面#####
+
+    # 輸入開課序號
+    input_classSerialNumber = driver.find_element_by_id("serialNo-inputEl")
+    input_classSerialNumber.send_keys( classSerialNumber )
+
+    # 開始查詢
+    driver.find_element_by_id("button-1059-btnInnerEl").click()
+    
+    # 點選課程
+    driver.find_element_by_id("grid-body").click()
+    driver.find_element_by_id("gridview-1123-body").click()
+
+    # 點選"課程大綱"
+    driver.find_element_by_id("book_open1-btnInnerEl").click()
+
+def selectCourseInNTU(driver, classSerialNumber):
+    # 基本資料
+    account = '40747043S'
+    password = 'cactus415646851'
+    login(driver, account, password)
+
+    # 刪除時間
+    time.sleep(2)
+    deleteTime = "document.getElementById('now').remove();"
+    driver.execute_script(deleteTime)
+
+    # 切換frame
+    time.sleep(2)
+    driver.switch_to.frame("stfseldListDo")
+    
     # 點選"查詢"
-    pyautogui.click(670, 620)
-    pyautogui.rightClick()
-    time.sleep(1)
-    pyautogui.press('n')
-    time.sleep(1)
-    pyautogui.rightClick()
-    time.sleep(1)
-    pyautogui.press('n')
-    time.sleep(1)
-"""
-    # 點選"查詢"
-    # driver.find_element_by_id("button-1059-btnInnerEl").click()
-"""
-    try:
-        driver.find_element_by_id("button-1059-btnInnerEl").click()
-    except NoSuchElementException:
-        print("error")
-"""
+    driver.find_element_by_id("query-btnInnerEl").click()
+
+    #####選課畫面#####
+
+    # 輸入開課序號
+    input_classSerialNumber = driver.find_element_by_id("serialNo-inputEl")
+    input_classSerialNumber.send_keys( classSerialNumber )
+
+    # 開始查詢
+    driver.find_element_by_id("button-1059-btnInnerEl").click()
+    
+    # 點選課程
+    driver.find_element_by_id("grid-body").click()
+    driver.find_element_by_id("gridview-1123-body").click()
+
+    # 點選"課程大綱"
+    driver.find_element_by_id("book_open1-btnInnerEl").click()
 
 
 if __name__ == '__main__':
     driver = openChrome()
 
-    selectCourseInNTNU(driver)
-    # time.sleep(10)
+    selectCourseInNTNU(driver, 3210)
+    time.sleep(10)
     
     # 印出抓到的
     # soup = BeautifulSoup(driver.page_source, "html.parser")
